39eb956513c667046c225f163c0475b38b28e1de
MaksOfKharkiv <maksim.zyuzik@gmail.com>
2021-11-28 12:26:23 +0200

    Added section Work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-$(document).ready(function () {}
+$(document).ready(function () {
+    $('.menu-button').on('click', function() {
+        $('.navigation-wrapper').addClass('active');
+    })
+    $('.close-menu').on('click', function() {
+        $('.navigation-wrapper').removeClass('active');
+    })
+})
